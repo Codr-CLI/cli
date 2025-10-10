2dfe28a4770761d09ea60737a0e5ae54dae99e8a
pshycodr <115141578+pshycodr@users.noreply.github.com>
2025-10-10 15:28:46 +0530

    refactor(generateCodebaseMetadata): uncommented non rag related features

--- apps/cli-ts/src/core/agents/codeAgent/tools/generateCodebaseMetadata.ts
+++ apps/cli-ts/src/core/agents/codeAgent/tools/generateCodebaseMetadata.ts
@@ -1,89 +1,89 @@
-// import { startLoader, stopLoader } from "@cli/ui/Loader/loaderManager";
-// import { parseCodebase } from "@codebase";
-// import generateCallGraph from "@core/context/codebaseMetaData/callgraph";
-// import generateClassMetadata from "@core/context/codebaseMetaData/classMetadata";
-// import { generateCssMetadata } from "@core/context/codebaseMetaData/cssMetadata";
-// import generateFileMetadata from "@core/context/codebaseMetaData/filesMetadata";
-// import generateFunctionMetadata from "@core/context/codebaseMetaData/functionMeta";
-// import { generateHtmlMetadata } from "@core/context/codebaseMetaData/htmlMetadata";
+import { startLoader, stopLoader } from "@cli/ui/Loader/loaderManager";
+import { parseCodebase } from "@codebase";
+import generateCallGraph from "@core/context/codebaseMetaData/callgraph";
+import generateClassMetadata from "@core/context/codebaseMetaData/classMetadata";
+import { generateCssMetadata } from "@core/context/codebaseMetaData/cssMetadata";
+import generateFileMetadata from "@core/context/codebaseMetaData/filesMetadata";
+import generateFunctionMetadata from "@core/context/codebaseMetaData/functionMeta";
+import { generateHtmlMetadata } from "@core/context/codebaseMetaData/htmlMetadata";
 // import callRAG, { RagClient } from "@transport/zeromqClient";
-// import chalk from "chalk";
-// import path from "path";
+import chalk from "chalk";
+import path from "path";
 
-// type CodeEntity = {
-// 	entity_name: string;
-// 	code: string;
-// 	entity_type: string;
-// 	file_path: string;
-// 	start_line: number;
-// 	end_line: number;
-// 	description?: string;
-// };
+type CodeEntity = {
+	entity_name: string;
+	code: string;
+	entity_type: string;
+	file_path: string;
+	start_line: number;
+	end_line: number;
+	description?: string;
+};
 
-// interface QueryData {
-// 	path: string;
-// 	query: "none";
-// 	parsedCodebase?: CodeEntity[];
-// 	type: "codebase";
-// }
+interface QueryData {
+	path: string;
+	query: "none";
+	parsedCodebase?: CodeEntity[];
+	type: "codebase";
+}
 
-// const metadataPath = path.resolve("./.codr/metadata");
+const metadataPath = path.resolve("./.codr/metadata");
 // const rag = new RagClient();
-// const dirname = process.cwd();
+const dirname = process.cwd();
 
-// const generateCodebaseMetadata = async () => {
-// 	startLoader(`Generating Codebase Metadata`);
-// 	try {
-// 		const { success, response } = await rag.callRagOnce({
-// 			type: "check_collection",
-// 			path: dirname,
-// 		});
+const generateCodebaseMetadata = async () => {
+	startLoader(`Generating Codebase Metadata`);
+	try {
+		// const { success, response } = await rag.callRagOnce({
+		// 	type: "check_collection",
+		// 	path: dirname,
+		// });
 
-// 		generateCallGraph();
-// 		generateClassMetadata();
-// 		generateFileMetadata();
-// 		generateFunctionMetadata();
-// 		generateHtmlMetadata();
-// 		generateCssMetadata();
+		generateCallGraph();
+		generateClassMetadata();
+		generateFileMetadata();
+		generateFunctionMetadata();
+		generateHtmlMetadata();
+		generateCssMetadata();
 
-// 		// if (fs.existsSync(metadataPath) && response.exists) {
-// 		//   startLoader("Checking .metadata folder existence");
-// 		//   stopLoader(".metadata folder exists ✅");
-// 		//   return "project .metadata folder already exists in the root folder and in the vector db";
-// 		// }
+		// if (fs.existsSync(metadataPath) && response.exists) {
+		//   startLoader("Checking .metadata folder existence");
+		//   stopLoader(".metadata folder exists ✅");
+		//   return "project .metadata folder already exists in the root folder and in the vector db";
+		// }
 
-// 		if (!response.exists) {
-// 			// Parse codebase
-// 			startLoader(
-// 				chalk.cyan("🔍 Parsing and summarizing codebase..., path: ") + dirname,
-// 			);
-// 			const parsed = await parseCodebase(dirname);
-// 			stopLoader(chalk.cyanBright("📄 Parsed: ") + parsed?.length + " items");
+		// if (!response.exists) {
+		// 	// Parse codebase
+		// 	startLoader(
+		// 		chalk.cyan("🔍 Parsing and summarizing codebase..., path: ") + dirname,
+		// 	);
+		// 	const parsed = await parseCodebase(dirname);
+		// 	stopLoader(chalk.cyanBright("📄 Parsed: ") + parsed?.length + " items");
 
-// 			// Send to RAG
-// 			startLoader(chalk.yellowBright("📡 Sending to RAG..."));
-// 			const data: QueryData = {
-// 				path: dirname,
-// 				parsedCodebase: parsed,
-// 				type: "codebase",
-// 				query: "none",
-// 			};
+		// 	// Send to RAG
+		// 	startLoader(chalk.yellowBright("📡 Sending to RAG..."));
+		// 	const data: QueryData = {
+		// 		path: dirname,
+		// 		parsedCodebase: parsed,
+		// 		type: "codebase",
+		// 		query: "none",
+		// 	};
 
-// 			const { success: ragSuccess, response: context } = await callRAG(data);
+		// 	const { success: ragSuccess, response: context } = await callRAG(data);
 
-// 			if (!ragSuccess) {
-// 				stopLoader("❌ Failed to retrieve context from RAG.");
-// 				return { success: false };
-// 			}
-// 		}
-// 		stopLoader(
-// 			"project .metadata folder and vector embeddings created successfully",
-// 		);
+		// 	if (!ragSuccess) {
+		// 		stopLoader("❌ Failed to retrieve context from RAG.");
+		// 		return { success: false };
+		// 	}
+		// }
+		// stopLoader(
+		// 	"project .metadata folder and vector embeddings created successfully",
+		// );
 
-// 		return "project .metadata folder and vector embeddings created successfully";
-// 	} catch (error: any) {
-// 		stopLoader("❌ Failed to generate metadata");
-// 	}
-// };
+		return "project .metadata folder and vector embeddings created successfully";
+	} catch (error: any) {
+		stopLoader("❌ Failed to generate metadata");
+	}
+};
 
-// export default generateCodebaseMetadata;
+export default generateCodebaseMetadata;
